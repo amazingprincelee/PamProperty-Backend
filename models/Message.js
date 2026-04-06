63b4cd7122ae87acea833ced251c5edaa378c84d
Prince Lee <prince_lee.nna85@ymail.com>
2026-04-06 06:53:41 +0100

    update message

--- models/Message.js
+++ models/Message.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
   sender:       { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  type:         { type: String, enum: ['text', 'date_proposal', 'image', 'video', 'document'], default: 'text' },
+  type:         { type: String, enum: ['text', 'date_proposal', 'image', 'video', 'document', 'visit_request'], default: 'text' },
   text:         { type: String, default: '' },
 
   // Attachment (image / video / document)
